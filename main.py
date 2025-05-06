1bb029581a99a8bc016938a14ac8708d7eb378b2
Matthew Richards <matthewrichards234@gmail.com>
2025-05-06 10:44:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,26 @@ html_document = getHTMLdocument(url_to_scrape)
 
 soup = BeautifulSoup(html_document, 'html.parser')
 
-#print(soup.prettify())
+links = [link for link in soup.find_all('a')]
+sub_urls = []
 
-for link in soup.find_all('a'): 
-    print(link.get('href'))   
+for link in links:
+    href = link.get('href')
+    #print(href)
+
+    # add genre to "catalogue/category/books/" !
+    # f.E: catalogue/category/books/horror
+
+    if "catalogue/category/books/" in href:
+        sub_urls.append(url_to_scrape + "catalogue/category/books/")
+
+
+for sub_url in sub_urls:
+    print(sub_url)
+    
+
+split_url = sub_urls[0].split('/') 
+print(split_url)
+idx_to_replace = split_url[-1]
+category_idx = split_url[6]
+print(category_idx)
